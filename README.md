d58153e02844713e74ec2499b3da31318ecc7e85
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2023-11-21 11:23:53 +0900

    initial

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # webgateway-examples
 Examples of how to configure and use the WebGateway with InterSystems IRIS
+
+Modified to meet my own purpose.
+
+# docker-compose 
+
+review [demo-compose/README.txt](README.txt) first.
+
+## prepare TLS files
+
+Create private/public keys for Apache Web Server.
+```
+$ git clone https://github.com/IRISMeister/apache-ssl.git
+$ cd apache-ssl
+$ ./setup.sh
+```
+
+Copy them into webgateway/ folder.
+```
+$ cd webgateway-examples/demo-compose
+$ cp ~/git/apache-ssl/ssl/server.crt webgateway/sslwebcert.pem
+$ cp ~/git/apache-ssl/ssl/server.key webgateway/sslwebkey.pem
+$ cp ~/git/apache-ssl/ssl/caint.crt webgateway/SSLCertificateChainFile.pem
+
+```
--- demo-compose/setup.sh
+++ demo-compose/setup.sh
@@ -1,8 +1,14 @@
 source parameters.cfg
 
+# use apache-ssl to create them. See README.md
+# cp ~/git/apache-ssl/ssl/server.crt webgateway/sslwebcert.pem
+# cp ~/git/apache-ssl/ssl/server.key webgateway/sslwebkey.pem
+# cp ~/git/apache-ssl/ssl/caint.crt webgateway/SSLCertificateChainFile.pem
+
 #; Prior to running the docker-compose, ensure the instances have
 #; read-write permissions to the bindmounts.
-chmod -R 755 iris
+#chmod -R 755 iris
+sudo chown -R 51773:51773 iris
 chmod -R 755 webgateway
 
 docker-compose build
@@ -12,5 +18,6 @@ docker-compose up -d
 # SSL SETUP - omit if opting out of SSL
 # It takes several seconds for iris to spin up, so wait for it
 # before configuring it for SSL. Adjust sleep time if need be.
-sleep 30
-docker exec iris /bin/sh -c "/iris-shared/configure.sh"
+#sleep 30
+#docker exec iris /bin/sh -c "/iris-shared/configure.sh"
+
